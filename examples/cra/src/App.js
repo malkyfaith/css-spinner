e8bf6b83ed86cd26db36cf8c6840007f03f96747
Malkeet Singh <malkeet.singh@lendi.com.au>
2019-06-11 23:38:51 +1000

    cra example

---
+++ examples/cra/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Ring, Ripple} from 'my-css-spinners';
+
+function App() {
+  return (
+    <>
+      <Ring />
+      <Ripple />
+    </>
+  );
+}
+
+export default App;
