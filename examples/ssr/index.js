951fbc103fd94710408d4b98c89db0da3eb21789
Malkeet Singh <malkeet.singh@lendi.com.au>
2019-06-11 23:39:21 +1000

    ssr example

---
+++ examples/ssr/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const { createElement, Fragment } = require('react')
+const { renderToString } = require('react-dom/server')
+const { Ring, Ripple } = require('my-css-spinners')
+
+const port = 3000
+const app = express()
+
+const html = renderToString(
+  createElement(Fragment, null, createElement(Ring), createElement(Ripple))
+)
+
+app.get('*', (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <title>CJS Example</title>
+    </head>
+    <body>
+      ${html}
+    </body>
+    </html>
+  `)
+})
+
+app.listen(port, () => console.log(`http://localhost:${port}`))
